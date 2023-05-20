731843ca1430a1d31f0007e59bb0400286a0acdf
reznuhasan <reznuhasan31@gmail.com>
2023-05-20 07:25:18 +0600

    add practice

---
+++ Explore Nodejs/Thapa Technical/myModule/index.js
@@ -0,0 +1,3 @@
+const add=require('./fun');
+
+console.log(add(5,5))
---
+++ Explore Nodejs/Thapa Technical/path/index.js
@@ -0,0 +1,6 @@
+const path=require('path');
+console.log(__dirname)
+console.log(path.dirname('D:/riznu/Backend/Explore Nodejs/Thapa Technical/path/index.js'));
+console.log(path.extname('D:/riznu/Backend/Explore Nodejs/Thapa Technical/path/index.js'));
+console.log(path.basename('D:/riznu/Backend/Explore Nodejs/Thapa Technical/path/index.js'));
+console.log(path.parse('D:/riznu/Backend/Explore Nodejs/Thapa Technical/path/index.js'));
