a12ea6e8e3469b01ad308fe13217379587cedb69
reznuhasan <reznuhasan31@gmail.com>
2023-05-26 04:47:27 +0600

    add edit

--- Mongoose/Products/routes/products.route.js
+++ Mongoose/Products/routes/products.route.js
@@ -5,27 +5,38 @@ const mongoose=require('mongoose');
 const product=mongoose.model('product',productSchema);
 //get multiple
 router.get('/',async(req,res)=>{
-    await product.find({}).exec()
-    .then(result=>res.status(200).json({message:"find successfully",result}))
-    .catch(err=>res.status(500).json({error:"server side error"}))
+    try{
+       const result= await product.find({}).exec()
+       res.status(200).json({message:"find successfully",result})
+    }catch(err){
+        res.status(500).json({error:"server side error"})
+    }
+    
+    
 })
 //get only one 
 router.get('/:id',async(req,res)=>{
-    await product.findOne({_id:req.params.id}).exec()
-    .then(result=>res.status(200).json({message:"findOne successfully",result}))
-    .catch(err=>res.status(500).json({error:"server side error"}))
+   try{
+    const result=await product.findOne({_id:req.params.id})
+    res.status(200).json({message:"find successfully",result})
+   }catch(err){
+    res.status(500).json({error:"server side error"})
+   }
 })
 //insert multiple data
 router.post('/all',async(req,res)=>{
-    await product.insertMany(req.body)
-    .then(result=>res.status(200).json({message:"insert successfully",result}))
-    .catch(err=>res.status(500).json({error:"server side error"}))
+   try{
+    const products= await product.insertMany(req.body)
+    res.status(200).json({message:"post multiple successfully",products})
+   }catch(err){
+    res.status(500).json({error:"server side error"})
+   }
 })
 //insert only one data
 router.post('/',async(req,res)=>{
     try{
         const newProduct=new product(req.body);
-        newProduct.save()
+        await newProduct.save()
         res.status(200).json({message:"product add successfully",newProduct})
     }
     catch(err){
@@ -34,15 +45,23 @@ router.post('/',async(req,res)=>{
 })
 //update only one data
 router.put('/:id',async(req,res)=>{
-    await product.updateOne({_id:req.params.id},{name:"macbook 2"})
-    .then((result)=>res.status(200).json({message:"UpdateOne successfully",result}))
-    .catch(err=>res.status(500).json({error:"server side error"}))
+    try{
+        const result=await product.updateOne({_id:req.params.id},{name:"macbook 2"})
+        res.status(200).json({message:"product update successfully",result})
+    }
+    catch(err){
+       res.status(500).json({error:"server side error"})
+    } 
 })
 //delete only one data
 router.delete('/:id',async(req,res)=>{
-    await product.deleteOne({_id:req.params.id})
-    .then((result)=>res.status(200).json({message:"deleteOne successfully",result}))
-    .catch(err=>res.status(500).json({error:"server side error"}))
+    try{
+        const result=await product.deleteOne({_id:req.params.id})
+        res.status(200).json({message:"product delete successfully",result})
+    }
+    catch(err){
+       res.status(500).json({error:"server side error"})
+    } 
 })
 
 module.exports=router;
