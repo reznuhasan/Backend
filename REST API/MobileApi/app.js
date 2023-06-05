e47259ea7c0ce63642e643a762b668b32e516e5d
reznuhasan <reznuhasan31@gmail.com>
2023-06-06 02:18:53 +0600

    mongoose connect successfully

--- REST API/MobileApi/app.js
+++ REST API/MobileApi/app.js
@@ -1,9 +1,17 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
-
+const mongoose=require("mongoose");
 app.use(cors());
 app.use(express.json());
+//connect mongoose
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/MOBILEAPI');
+  console.log("server connect successfully");
+}
+
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     res.send("Welcome to my rest api");
