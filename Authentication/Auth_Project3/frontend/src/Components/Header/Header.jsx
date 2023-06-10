4c74886c5e8643a8fb334ab9bb1debf2c9ec2ed9
reznuhasan <reznuhasan31@gmail.com>
2023-06-11 03:35:53 +0600

    add register page with server

--- Authentication/Auth_Project3/frontend/src/Components/Header/Header.jsx
+++ Authentication/Auth_Project3/frontend/src/Components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from "react-router-dom";
-
+import './Header.css'
+const classNameFunc=({isActive})=>(isActive)?"active_link":"";
 const Header=()=>{
     return(
-        <div>
+        <div className="nav">
             <NavLink to="/"
-            
-            >Home</NavLink>
+            className={classNameFunc}
+            end>Home</NavLink>
             <NavLink to="/register"
+            className={classNameFunc}
             >Register</NavLink>
             <NavLink to="/login"
+            className={classNameFunc}
             >Login</NavLink>
         </div>
     )
--- Authentication/Auth_Project3/frontend/src/Components/Register/Register.jsx
+++ Authentication/Auth_Project3/frontend/src/Components/Register/Register.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import './Register.css'
 const Register=()=>{
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const handleName=(e)=>{
+        setName(e.target.value);
+    }
+    const handleEmail=(e)=>{
+        setEmail(e.target.value);
+    }
+    const handlePassword=(e)=>{
+        setPassword(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const newUser={
+            name:name,
+            email:email,
+            password:password
+        }
+        setName("");
+        setEmail("");
+        setPassword("")
+        fetch('http://localhost:5000/register',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(newUser),
+        }).then((response) => response.json())
+        .then((responseData) => {
+          console.log("Data sent to server:", responseData);
+        })
+        .catch((error) => {
+          console.log("Error sending data to server:", error);
+        });
+        
+    }
     return(
-        <div>
-            <h1>This is my Register Page</h1>
+        <div className="register">
+            <h1>Register Form</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input type="text" value={name} placeholder="enter your name" onChange={handleName}/>
+                <label>Email:</label>
+                <input type="email" value={email} placeholder="enter your email" onChange={handleEmail}/>
+                <label>Password:</label>
+                <input type="password" value={password} placeholder="enter your password" onChange={handlePassword}/>
+                <input type="submit" value="submit"/>
+            </form>
         </div>
     )
 }
