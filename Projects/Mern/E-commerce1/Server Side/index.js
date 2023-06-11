4699087e944e61edc8e280d9b40190443100847c
reznuhasan <reznuhasan31@gmail.com>
2023-06-12 01:56:12 +0600

    server create

---
+++ Projects/Mern/E-commerce1/Server Side/index.js
@@ -0,0 +1,7 @@
+const app=require('./app');
+require('dotenv').config();
+const port =process.env.PORT || 8000
+
+app.listen(port,()=>{
+    console.log(`server running successfully at http://localhost:${port}`);
+})
