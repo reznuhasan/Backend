5a0a329d04c0f27f899c4fe0cc71933e6f523ead
reznuhasan <reznuhasan31@gmail.com>
2023-06-21 05:28:04 +0600

    add tailwind product list

---
+++ Projects/Mern/E-commerce2/client-side-ecommerce/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ProductList from './features/product-list/ProductList'
+
+function App() {
+
+  return (
+    <>
+     <ProductList/>
+    </>
+  )
+}
+
+export default App
