e8bae6048e9d85dd9cd66f3c95774a9cebdf9008
reznuhasan <reznuhasan31@gmail.com>
2023-06-21 07:24:25 +0600

    login and signup style done

--- Projects/Mern/E-commerce2/client-side-ecommerce/src/App.jsx
+++ Projects/Mern/E-commerce2/client-side-ecommerce/src/App.jsx
@@ -2,12 +2,16 @@
 import Navbar from './features/navbar/Navbar'
 import ProductList from './features/product-list/ProductList'
 import Home from './pages/Home'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
 
   return (
     <>
-      <Home/>
+     {/* <SignupPage/> */}
+      <LoginPage/>
+      {/* <Home/> */}
     </>
   )
 }
