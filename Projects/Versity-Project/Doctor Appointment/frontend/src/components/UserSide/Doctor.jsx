c9b7789d41a6dd0f8c8bf038d916f7aa6ceb6486
reznuhasan <reznuhasan31@gmail.com>
2023-07-30 03:14:47 +0600

    add doctor part

--- Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Doctor.jsx
+++ Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Doctor.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { Card, Col } from 'react-bootstrap'
-
-const Doctor = ({doctor}) => {
+import { Button, Card, Col } from 'react-bootstrap'
+import styles from "../../styles/Doctor.module.css"
+import { Link } from 'react-router-dom';
+const Doctor = ({ doctor }) => {
     console.log(doctor);
-    const {image,name,qualification,category,fees,hospitals}=doctor
+    const { image, name, qualification, category, fees, hospitals } = doctor
     return (
         <div>
             <Col>
                 <Card>
-                    <Card.Img variant="top" src={`/src/assets/${image}`}/>
+                    <Card.Img variant="top" src={`/src/assets/${image}`} />
                     <Card.Body>
                         <Card.Title>{name}</Card.Title>
                         <Card.Text>
-                            qualification{}
+                            <div className={styles.doctorContent}>
+                                <span>Qualification : </span>&nbsp;<p>{qualification}</p>
+                            </div>
+                            <div className={styles.doctorContent}>
+                                <span>Category : </span>&nbsp;<p>{category}</p>
+                            </div>
+                            <div className={styles.doctorContent}>
+                                <span>Hospitals : </span>&nbsp;<p>{hospitals[0].name}</p>
+                            </div>
+                            <div className={styles.doctorContent}>
+                                <span>Time : </span>&nbsp;<p>{hospitals[0].timeSlots[0].day} [{hospitals[0].timeSlots[0].startTime} to {hospitals[0].timeSlots[0].endTime}]</p>
+                            </div>
+                            <div className={styles.doctorContent}>
+                                <h5 style={{"color":"red"}}>Fees&nbsp;:&nbsp;${fees}</h5>
+                            </div>
                         </Card.Text>
+                        <Button className={styles.btn}><Link to={`/doctors/${name}`} className={styles.btnText}>Make an Appointment</Link></Button>
                     </Card.Body>
                 </Card>
             </Col>
--- Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/DoctorDetails.jsx
+++ Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/DoctorDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DoctorDetails = () => {
+    const {name}=useParams()
   return (
     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quibusdam cumque, fuga dolore libero ex? Ut eos neque placeat dolorem voluptas aut vitae, quisquam expedita, molestiae aperiam minus. Placeat nulla tempore dolorem ad adipisci voluptate quae consectetur aliquid, labore officiis mollitia expedita.
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit expedita omnis illo, repellendus esse ex nesciunt totam ipsam aut dolore temporibus ducimus suscipit. Qui iure fuga sint nemo nobis facilis?
