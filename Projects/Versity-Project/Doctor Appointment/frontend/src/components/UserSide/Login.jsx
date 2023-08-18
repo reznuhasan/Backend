c10bacfbd0c6c8e2a26f666daeb2baba41e34092
reznuhasan <reznuhasan31@gmail.com>
2023-08-19 00:26:33 +0600

    login register complete

--- Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Login.jsx
+++ Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Login.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container,Form,Row,Col} from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Card} from 'react-bootstrap';
 import { Image} from 'react-bootstrap';
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink,Link, useNavigate } from 'react-router-dom';
 import "../../styles/login.css"
+import axios from 'axios';
 
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+
+
+  const handleChangeInput = (e) => {
+    setUserData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+ 
+  useEffect(()=>{
+    const auth=localStorage.getItem("token")
+    if(auth){
+      navigate('/doctors')
+    }
+  },[])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/users/login', userData)
+      .then((response) => {
+        localStorage.setItem("token",response.data.token)
+        console.log(response.data)
+        console.log('User logged in successfully',);
+        // Update the authentication state to true
+        navigate("/doctors"); // Redirect to a protected route after successful login
+      })
+      .catch((err) => {
+        console.error('Error logging in:', err);
+        // Add proper error handling and display error messages to the user
+        alert('Login failed. Please try again.');
+      });
+  };
   return (
     <>
    <Container className='login' >
@@ -20,15 +58,15 @@ const Login = () => {
         <Col style={{ marginTop: '90px' }} xs={6} >
        <Card style={{ width: '20rem' }}>
          <Card.Body>
-         <Form >
+         <Form onSubmit={handleSubmit} >
            <Form.Group className="mb-3" controlId="formBasicEmail">
              {/* <Form.Label>Email address</Form.Label> */}
-             <Form.Control type="email" placeholder="Email Address" />
+             <Form.Control type="email" placeholder="Email Address" name='email' onChange={handleChangeInput} value={userData.email}/>
            </Form.Group>
 
            <Form.Group className="mb-3" controlId="formBasicPassword">
              {/* <Form.Label>Password</Form.Label> */}
-             <Form.Control type="password" placeholder="Password" />
+             <Form.Control type="password" placeholder="Password" name='password' onChange={handleChangeInput} value={userData.password} />
            </Form.Group>
            <Button style={{ width: '285px', marginTop:'10px' }} variant="primary" type="submit">
                     Signin
--- Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Register.jsx
+++ Projects/Versity-Project/Doctor Appointment/frontend/src/components/UserSide/Register.jsx
@@ -1,80 +1,173 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap';
-import { Image} from 'react-bootstrap';
-import { Container,Form,Row,Col} from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
+import { Container, Form, Row, Col } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 const Register = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const auth = localStorage.getItem("token")
+    if (auth) {
+      navigate('/doctors')
+    }
+  }, [])
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone: "",
+    gender: "Male",
+    age: "",
+    blood: "A+",
+  })
+  const handleChangeInput = (e) => {
+    setUserData(prevData => ({
+      ...prevData,
+      [e.target.name]: e.target.value
+    }))
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userData);
+    axios.post('http://localhost:5000/api/users/register', userData)
+      .then((response) => {
+        navigate('/login')
+        console.log('Registration successful!', response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the POST request
+        console.error('Registration failed:', error);
+      });
+    setUserData({
+      username: "",
+      email: "",
+      password: "",
+      phone: "",
+      gender: "Male",
+      age: "",
+      blood: "A+",
+    })
+  };
   return (
     <>
-    <Container style={{marginTop: '70px'}} >
-         <Row>
-          <Col style={{marginTop: '30px'}} xs={6}>
-          <Image style={{height:'100%', width: '100%'}} src="./assets/gif/registration2.gif" rounded />
+      <Container style={{ marginTop: '70px' }}>
+        <Row>
+          <Col style={{ marginTop: '30px' }} xs={6}>
+            <Image
+              style={{ height: '100%', width: '100%' }}
+              src="./assets/gif/registration2.gif"
+              rounded
+            />
           </Col>
-          <Col style={{marginTop:'80px'}} xs={6}>
-                <Form>
-                  <Row className="mb-2">
-                      <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>User Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User Name" />
-                      </Form.Group>
+          <Col style={{ marginTop: '80px' }} xs={6}>
+            <Form onSubmit={handleSubmit}>
+              <Row className="mb-2">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>User Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter User Name"
+                    name='username'
+                    value={userData.username}
+                    onChange={handleChangeInput}
+                  />
+                </Form.Group>
 
-                      <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Your Email" />
-                      </Form.Group>
-                  </Row>
-                  <Row className="mb-2">
-                      <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Password" />
-                      </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter Your Email"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleChangeInput}
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-2">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    value={userData.password}
+                    onChange={handleChangeInput}
+                  />
+                </Form.Group>
 
-                      <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Phone</Form.Label>
-                        <Form.Control type="tel" placeholder="Enter Your Number " />
-                      </Form.Group>
-                  </Row>
-                  <Row className="mb-2">
-                  <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Gender</Form.Label>
-                    <Form.Select defaultValue="Choose...">
-                      <option>Male</option>
-                      <option>Female</option>
-                      <option>Custom</option>
-                    </Form.Select>
-                  </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Phone</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    placeholder="Enter Your Number"
+                    name="phone"
+                    value={userData.phone}
+                    onChange={handleChangeInput}
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-2">
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Gender</Form.Label>
+                  <Form.Control
+                    as="select"
+                    name="gender"
+                    value={userData.gender}
+                    onChange={handleChangeInput}
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Custom">Custom</option>
+                  </Form.Control>
+                </Form.Group>
 
-                      <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Age</Form.Label>
-                        <Form.Control type="number" placeholder="Enter Your Age" />
-                      </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Age</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter Your Age"
+                    name="age"
+                    value={userData.age}
+                    onChange={handleChangeInput}
+                  />
+                </Form.Group>
 
-                      <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Blood Group</Form.Label>
-                    <Form.Select defaultValue="Choose...">
-                      <option>A+</option>
-                      <option>B+</option>
-                      <option>AB+</option>
-                      <option>AB-</option>
-                      <option>O+</option>
-                      <option>O-</option>
-                    </Form.Select>
-                  </Form.Group>
-
-
-                  </Row>
-                  <Button style={{ width: '260px', marginTop:'20px' }} variant="primary" type="submit">
-                    Signup
-                </Button>
-                <p style={{marginTop:'10px' }}>Already Have Account <NavLink to="/login">Signin</NavLink></p>
-                </Form>
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Blood Group</Form.Label>
+                  <Form.Control
+                    as="select"
+                    name="blood"
+                    value={userData.blood}
+                    onChange={handleChangeInput}
+                  >
+                    <option value="A+">A+</option>
+                    <option value="B+">B+</option>
+                    <option value="AB+">AB+</option>
+                    <option value="AB-">AB-</option>
+                    <option value="O+">O+</option>
+                    <option value="O-">O-</option>
+                  </Form.Control>
+                </Form.Group>
+              </Row>
+              <Button
+                style={{ width: '260px', marginTop: '20px' }}
+                variant="primary"
+                type="submit"
+              >
+                Signup
+              </Button>
+              <p style={{ marginTop: '10px' }}>
+                Already Have an Account? <NavLink to="/login">Sign In</NavLink>
+              </p>
+            </Form>
           </Col>
-      </Row>
-    </Container>
-    <hr></hr>
+        </Row>
+      </Container>
+      <hr />
     </>
   )
 }
