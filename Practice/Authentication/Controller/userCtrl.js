53fb03845e25a0479f6e1453b4205875934e498f
reznuhasan <reznuhasan31@gmail.com>
2023-08-28 06:31:25 +0600

    delete part add successfully

--- Practice/Authentication/Controller/userCtrl.js
+++ Practice/Authentication/Controller/userCtrl.js
@@ -35,13 +35,29 @@ const postUser=async(req,res,next)=>{
 
 }
 const putUserById=(req,res,next)=>{
-    
+    try{
+        const userId=req.params.userId;
+
+    }catch(err){
+        next(err)
+    }
 }
 const patchUserById=(req,res,next)=>{
     
 }
-const deleteUserById=(req,res,next)=>{
-    
+const deleteUserById=async(req,res,next)=>{
+    try{
+        const userId=req.params.userId;
+        const user=await findByProperty("_id",userId);
+        console.log(user)
+        if(!user){
+            throw customError("User is not available",400);
+        }
+        await user.deleteOne();
+        res.status(203).json({message:"user delete successfully"})
+    }catch(err){
+        next(err)
+    }
 }
 
 module.exports={
