fd44742cc7a32009a01208cf655f498413d20f58
reznuhasan <reznuhasan31@gmail.com>
2023-11-27 03:00:39 +0600

    cloudinary image upload successfully

--- Experiment/ImageUpload1/Backend/src/controllers/user.ctrl.js
+++ Experiment/ImageUpload1/Backend/src/controllers/user.ctrl.js
@@ -1,8 +1,20 @@
+import { cloudinaryUpload } from "../utlis/cloudinary.utlis.js";
 
 
 const registerUser=async(req,res)=>{
     try {
-        const {name,email,password,profile}=req.body
+        const {name,email,password}=req.body
+        console.log(name,email,password);
+        const profilePath=req.file.path
+        if(!profilePath){
+            res.status(400).json({error:"profile must be required"})
+        }
+        console.log(profilePath)
+        // const profileUrl=await cloudinaryUpload(profilePath);
+        // if(!profilePath){
+        //     res.status(400).json({error:"profile must be required"})
+        // }
+        res.status(200).json({message:"user create successfully"})
     } catch (error) {
         res.status(400).json({error:"authentication error",error})
     }
--- Experiment/ImageUpload1/Backend/src/routes/user.route.js
+++ Experiment/ImageUpload1/Backend/src/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/user.ctrl.js";
+import { upload } from "../middleware/multer.middleware.js";
 const router=express.Router();
 
-router.post('/register',registerUser)
+router.post('/register',upload.single('profile'),registerUser)
 
 export default router;
